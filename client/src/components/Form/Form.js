27d490c7e76f5f67d4a5d03a10edb19ec0319adb
MOD <mouhcinedafiaa@gmail.com>
2022-09-11 23:27:16 +0100

    Add client

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+import useStyles from './style';
+
+
+
+const Form = () => {
+    const classes = useStyles();
+    return (
+        <h1>from</h1>
+    );
+}
+
+export default Form;
---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,15 @@
+import React from "react";
+import useStyles from './style';
+
+
+const Post = () => {
+    const classes = useStyles();
+
+    return (
+        <h1>POSTS</h1>
+    );
+}
+
+
+export default Post
+
